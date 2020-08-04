989d1e9de550b9a16c6b568f0fee859771313d20
HouZhiGuoSir <1450935456@qq.com>
2020-08-04 21:40:27 +0800

    判断账户

--- android/src/main/java/name/avioli/unilinks/UniLinksPlugin.java
+++ android/src/main/java/name/avioli/unilinks/UniLinksPlugin.java
@@ -13,13 +13,13 @@ import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry;
-
+import java.text.SimpleDateFormat;
 public class UniLinksPlugin
         implements FlutterPlugin,
-                MethodChannel.MethodCallHandler,
-                EventChannel.StreamHandler,
-                ActivityAware,
-                PluginRegistry.NewIntentListener {
+        MethodChannel.MethodCallHandler,
+        EventChannel.StreamHandler,
+        ActivityAware,
+        PluginRegistry.NewIntentListener {
 
     private static final String MESSAGES_CHANNEL = "uni_links/messages";
     private static final String EVENTS_CHANNEL = "uni_links/events";
@@ -34,29 +34,60 @@ public class UniLinksPlugin
     private void handleIntent(Context context, Intent intent) {
         String action = intent.getAction();
         String dataString = intent.getDataString();
-        String aaa=intent.getExtras().getString("account");
-        Log.i("sichangyongaaa", "handleIntenta: "+aaa);
+//        Long  a=intent.getExtras().getLong("dispatchTime");
+//        Log.i("sichangyong", "-s: "+a);
+//        if (a!=null){
+//            String date = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss").format(
+//                    new java.util.Date(a));
+//            Log.i("sichangyong", "-s: "+date);
+//        }
+        //如果这个能拿到值就是uri跳转
+        String account=intent.getExtras().getString("account");
+        Log.i("sichangyong", "-1: "+account);
+        Log.i("sichangyong", "-1: "+dataString);
+        if (account!=null){
+            dataString = "holo://holoview-lab.com/?"
+                    + "&userName=" + intent.getExtras().getString("userName")
+                    + "&account=" + intent.getExtras().getString("account")
+                    + "&nonce=" +  intent.getExtras().getString("nonce")
+                    + "&timeStamp=" + intent.getExtras().getString("timeStamp")
+                    + "&signature=" +  intent.getExtras().getString("signature")
+                    + "&workNumber=" +  intent.getExtras().getString("workNumber")
+                    + "&dispatchTime=" +  intent.getExtras().getString("dispatchTime")
+                    + "&workType=" +  intent.getExtras().getString("workType")
+                    + "&faultType=" +  intent.getExtras().getString("faultType")
+                    + "&faultLoc=" +  intent.getExtras().getString("faultLoc")
+                    + "&faultDesc=" +  intent.getExtras().getString("faultDesc")
+                    + "&contactName=" +  intent.getExtras().getString("contactName")
+                    + "&contactMobile=" +  intent.getExtras().getString("contactMobile")
+                    + "&workStatus=" +  intent.getExtras().getString("workStatus")
+                    + "&extra=" + null;
+            Log.i("sichangyong", "-1: "+dataString);
+        }
         if (dataString == null) {
+            //包名跳转map传参
             HashMap<String, String> map = (HashMap<String, String>) intent.getSerializableExtra("map");
-            Log.i("sichangyong", "handleIntent: "+map);
+            Log.i("sichangyong", "1: "+map);
             if (map!=null){
                 dataString = "holo://holoview-lab.com/?"
-                + "&userName=" + map.get("userName")
-                + "&account=" + map.get("account")
-                + "&nonce=" +  map.get("nonce")
-                + "&timeStamp=" + map.get("timeStamp")
-                + "&signature=" +  map.get("signature")
-                + "&workNumber=" +  map.get("workNumber")
-                + "&dispatchTime=" +  map.get("dispatchTime")
-                + "&workType=" +  map.get("workType")
-                + "&faultType=" +  map.get("faultType")
-                + "&faultLoc=" +  map.get("faultLoc")
-                + "&faultDesc=" +  map.get("faultDesc")
-                + "&contactName=" +  map.get("contactName")
-                + "&contactMobile=" +  map.get("contactMobile")
-                + "&workStatus=" +  map.get("workStatus")
-                + "&extra=" + null;
+                        + "&userName=" + map.get("userName")
+                        + "&account=" + map.get("account")
+                        + "&nonce=" +  map.get("nonce")
+                        + "&timeStamp=" + map.get("timeStamp")
+                        + "&signature=" +  map.get("signature")
+                        + "&workNumber=" +  map.get("workNumber")
+                        + "&dispatchTime=" +  map.get("dispatchTime")
+                        + "&workType=" +  map.get("workType")
+                        + "&faultType=" +  map.get("faultType")
+                        + "&faultLoc=" +  map.get("faultLoc")
+                        + "&faultDesc=" +  map.get("faultDesc")
+                        + "&contactName=" +  map.get("contactName")
+                        + "&contactMobile=" +  map.get("contactMobile")
+                        + "&workStatus=" +  map.get("workStatus")
+                        + "&extra=" + null;
+                Log.i("sichangyong", "2: "+dataString);
             }else{
+                //包名跳转extra传参数
                 dataString = "holo://holoview-lab.com/?"
                         + "&userName=" + intent.getExtras().getString("userName")
                         + "&account=" + intent.getExtras().getString("account")
@@ -73,11 +104,12 @@ public class UniLinksPlugin
                         + "&contactMobile=" +  intent.getExtras().getString("contactMobile")
                         + "&workStatus=" +  intent.getExtras().getString("workStatus")
                         + "&extra=" + null;
-                Log.i("sichangyongaaa", "handleIntenta: "+dataString);
+                Log.i("sichangyong", "3: "+dataString);
             }
         }
+        Log.i("sichangyong", "4: "+dataString);
+        Log.i("sichangyong", "5: "+action);
         if (Intent.ACTION_VIEW.equals(action)) {
-
             if (initialIntent) {
                 initialLink = dataString;
                 initialIntent = false;
@@ -96,9 +128,33 @@ public class UniLinksPlugin
                 // Log.v("uni_links", String.format("received action: %s", intent.getAction()));
 
                 String dataString = intent.getDataString();
+
+                String account=intent.getExtras().getString("account");
+                Log.i("sichangyong", "-11: "+account);
+                Log.i("sichangyong", "-11: "+dataString);
+                if (account!=null){
+                    dataString = "holo://holoview-lab.com/?"
+                            + "&userName=" + intent.getExtras().getString("userName")
+                            + "&account=" + intent.getExtras().getString("account")
+                            + "&nonce=" +  intent.getExtras().getString("nonce")
+                            + "&timeStamp=" + intent.getExtras().getString("timeStamp")
+                            + "&signature=" +  intent.getExtras().getString("signature")
+                            + "&workNumber=" +  intent.getExtras().getString("workNumber")
+                            + "&dispatchTime=" +  intent.getExtras().getString("dispatchTime")
+                            + "&workType=" +  intent.getExtras().getString("workType")
+                            + "&faultType=" +  intent.getExtras().getString("faultType")
+                            + "&faultLoc=" +  intent.getExtras().getString("faultLoc")
+                            + "&faultDesc=" +  intent.getExtras().getString("faultDesc")
+                            + "&contactName=" +  intent.getExtras().getString("contactName")
+                            + "&contactMobile=" +  intent.getExtras().getString("contactMobile")
+                            + "&workStatus=" +  intent.getExtras().getString("workStatus")
+                            + "&extra=" + null;
+                    Log.i("sichangyong", "-11: "+dataString);
+                }
+
                 if (dataString == null) {
                     HashMap<String, String> map = (HashMap<String, String>) intent.getSerializableExtra("map");
-                    Log.i("sichangyong", "handleIntent: "+map);
+                    Log.i("sichangyong", "11: "+map);
                     if (map!=null){
                         dataString = "holo://holoview-lab.com/?"
                                 + "&userName=" + map.get("userName")
@@ -116,6 +172,7 @@ public class UniLinksPlugin
                                 + "&contactMobile=" +  map.get("contactMobile")
                                 + "&workStatus=" +  map.get("workStatus")
                                 + "&extra=" + null;
+                        Log.i("sichangyong", "22: "+dataString);
 
                     }else{
                         dataString = "holo://holoview-lab.com/?"
@@ -134,7 +191,7 @@ public class UniLinksPlugin
                                 + "&contactMobile=" +  intent.getExtras().getString("contactMobile")
                                 + "&workStatus=" +  intent.getExtras().getString("workStatus")
                                 + "&extra=" + null;
-                        Log.i("sichangyongaaa", "handleIntenta: "+dataString);
+                        Log.i("sichangyong", "33: "+dataString);
                     }
                 }
                 if (dataString == null) {
